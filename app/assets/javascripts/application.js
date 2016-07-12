ded0359b348e931a24b835480455d29593b6b795
Florian Maneyrol <florianmaneyrol@gmail.com>
2016-07-12 17:30:05 +0200

    new seance form done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,42 @@
 //= require_tree .
 
 
+// $(".endstep1").click(function(){
+// 		$(".step1").addClass("hidden");
+// 		$(".step2").removeClass('hidden');
+// 		$(".endstep2").removeClass('hidden');
+// 		$(this).addClass('hidden');
+
+
+// });
+
+// $(".endstep2").click(function(){
+// 		$(".step2").addClass("hidden");
+// 		$(".step3").removeClass('hidden');
+// 		$(".endstep3").removeClass('hidden');
+// 		$(this).addClass('hidden');
+
+// });
+
+// $(".endstep3").click(function(){
+// 		$(".step3").addClass("hidden");
+// 		$(".step4").removeClass('hidden');
+// 		$(".endstep4").removeClass('hidden');
+// 		$(this).addClass('hidden');
+
+// });
+
+// $(".endstep4").click(function(){
+// 		$(".step4").addClass("hidden");
+// 		$(".step5").removeClass('hidden');
+// 		$(".endstep5").removeClass('hidden');
+// 		$(this).addClass('hidden');
+// });
+
+// $(".endstep5").click(function(){
+// 		$(".step5").addClass("hidden");
+// 		$(".step6").removeClass('hidden');
+// 		$(".submit").removeClass('hidden');
+// 		$(this).addClass('hidden');
+// });
+
